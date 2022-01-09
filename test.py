74e83d5e856cb68855c17926489bb4700597688d
epalazli <palazli@web.de>
2022-01-09 16:08:07 +0100

    test file deleted

--- test.py
+++
@@ -1,9 +0,0 @@
-from deep_translator import GoogleTranslator
-from deep_translator import PonsTranslator
-
-to_translate = "Sevgili kardeşim, seni çok seviyorum!"
-translated = PonsTranslator(source='english',
-target='german').translate(text, to_translate)
-
-print(translated)
-
